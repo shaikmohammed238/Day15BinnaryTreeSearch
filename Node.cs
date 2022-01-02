56d28e0ff4b0479f5fb87daf522377a28ac1889a
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-01-02 20:45:50 +0530

    added UC1bts

---
+++ Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day15_binary_tree
+{
+    class Node
+    {
+        public int data;
+        public Node LeftNode;
+        public Node RightNode;
+
+        public Node()
+        {
+            this.LeftNode = null;
+            this.RightNode = null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace day15_binary_tree
+{
+    class Program
+    {
+        //Ability to create a BST by adding 56 and then adding 30 & 70 - Use INode to create My Binary Node 
+        static void Main(string[] args)
+        {
+            BinaryTree obj1 = new BinaryTree();
+            obj1.Insert(56);
+            obj1.Insert(30);
+            obj1.Insert(70);
+            obj1.TraversePreorder(obj1.root);
+        }
+    }
+}
